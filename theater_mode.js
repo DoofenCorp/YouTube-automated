8ebdf146d1d18d0b6942b25ab7ed7e71eb442a5e
DoofenCorp <48542507+DoofenCorp@users.noreply.github.com>
2023-07-02 00:27:18 +0530

    theater mode support

---
+++ theater_mode.js
@@ -0,0 +1,2 @@
+var theater_mode_button = document.getElementsByClassName("ytp-size-button")[0];
+theater_mode_button.click();
--- themeChanger.js
+++ themeChanger.js
@@ -3,19 +3,27 @@ settings_menu = async () => {
   yt_settings = document.querySelector("ytd-topbar-menu-button-renderer");
   yt_settings.click();
   yt_settings.click();
-  yt_settings.click();
 };
 await settings_menu();
 
-var theme_button = document.getElementsByTagName(
-  "ytd-toggle-theme-compact-link-renderer"
-)[0];
-theme_button.click();
-var link_renderers = document.getElementsByTagName("ytd-compact-link-renderer");
-for (var i = 0; i < link_renderers.length; i++) {
-  var label = link_renderers[i].getElementsByTagName("yt-formatted-string")[0]
-    .innerHTML;
-  if (label === "Dark theme") {
-    link_renderers[i].click();
+var theme_button;
+theme_change = async () => {
+  theme_button = document.getElementsByTagName(
+    "ytd-toggle-theme-compact-link-renderer"
+  )[0];
+  theme_button.click();
+};
+await theme_change();
+
+var link_renderers;
+change_theme = async () => {
+  link_renderers = document.getElementsByTagName("ytd-compact-link-renderer");
+  for (var i = 0; i < link_renderers.length; i++) {
+    var label = link_renderers[i].getElementsByTagName("yt-formatted-string")[0]
+      .innerHTML;
+    if (label === "Dark theme") {
+      link_renderers[i].click();
+    }
   }
-}
+};
+await change_theme();
