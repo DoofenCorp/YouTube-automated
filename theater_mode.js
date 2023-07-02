10862595d28c714ddc01ea220fe99309b095a823
DoofenCorp <48542507+DoofenCorp@users.noreply.github.com>
2023-07-03 02:35:23 +0530

    Mutation observer implemented in theme changer

--- theater_mode.js
+++ theater_mode.js
@@ -1,2 +1,3 @@
+console.log("Theater mode running");
 var theater_mode_button = document.getElementsByClassName("ytp-size-button")[0];
 theater_mode_button.click();
--- themeChanger.js
+++ themeChanger.js
@@ -1,33 +1,50 @@
-var yt_settings;
-settings_menu = async () => {
-  yt_settings = document.querySelector("ytd-topbar-menu-button-renderer");
-  yt_settings.click();
-  while (document.querySelector("ytd-toggle-theme-compact-link-renderer") === null) {
-    console.log("waiting");
-    await new Promise(resolve => setTimeout(resolve, 100));
-  }
-  yt_settings.click();
-};
-await settings_menu();
+function waitForElement(selector) {
+  return new Promise((resolve) => {
+    const observer = new MutationObserver((mutationsList, observer) => {
+      const element = document.querySelector(selector);
+      if (element) {
+        observer.disconnect();
+        resolve(element);
+      }
+    });
 
-var theme_button;
-theme_change = async () => {
-  theme_button = document.getElementsByTagName(
-    "ytd-toggle-theme-compact-link-renderer"
-  )[0];
-  theme_button.click();
-};
-await theme_change();
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
 
-var link_renderers;
-change_theme = async () => {
-  link_renderers = document.getElementsByTagName("ytd-compact-link-renderer");
-  for (var i = 0; i < link_renderers.length; i++) {
-    var label = link_renderers[i].getElementsByTagName("yt-formatted-string")[0]
-      .innerHTML;
-    if (label === "Dark theme") {
-      link_renderers[i].click();
+(async () => {
+  var yt_settings;
+  settings_menu = async () => {
+    yt_settings = document.querySelector("ytd-topbar-menu-button-renderer");
+    yt_settings.click();
+    await waitForElement("ytd-toggle-theme-compact-link-renderer");
+    yt_settings.click();
+  };
+  await settings_menu();
+
+  var theme_button;
+  theme_change = () => {
+    theme_button = document.querySelector(
+      "ytd-toggle-theme-compact-link-renderer"
+    );
+    theme_button.click();
+  };
+  theme_change();
+
+  var link_renderers;
+  change_theme = () => {
+    link_renderers = document.getElementsByTagName("ytd-compact-link-renderer");
+    for (var i = 0; i < link_renderers.length; i++) {
+      var label = link_renderers[i].getElementsByTagName(
+        "yt-formatted-string"
+      )[0].innerHTML;
+      if (label === "Dark theme") {
+        link_renderers[i].click();
+      }
     }
-  }
-};
-await change_theme();
+  };
+  change_theme();
+})();
